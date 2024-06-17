cb372068d2a118fde0ed78791ec9ec70b5eaf218
yakuhito <y@kuhi.to>
2024-06-17 11:31:55 +0300

    data store info

---
+++ src/puzzles_info.rs
@@ -0,0 +1,23 @@
+use chia_protocol::{Bytes32, Coin};
+use chia_puzzles::Proof;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[must_use]
+pub enum DelegatedPuzzle {
+    Admin(Bytes32),
+    Writer(Bytes32),
+    NoFilter(Bytes32),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[must_use]
+pub struct DataStoreInfo<M> {
+    pub launcher_id: Bytes32,
+    pub coin: Coin,
+    pub proof: Proof,
+    pub metadata: M,
+    pub owner_puzzle_hash: Bytes32,
+    pub oracle_address: Option<Bytes32>,
+    pub oracle_fee: Option<u64>,
+    pub delegated_puzzles: Option<Vec<DelegatedPuzzle>>,
+}
