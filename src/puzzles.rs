664df36d39a62c6ea3fb3f70eb6126909d85ac91
yakuhito <y@kuhi.to>
2024-07-20 17:12:52 +0300

    use the new puzzles and update puzzle hashes

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -48,7 +48,7 @@ pub struct DelegationLayerSolution<P, S> {
   pub puzzle_solution: S,
 }
 
-pub const DELEGATION_LAYER_PUZZLE: [u8; 904] = hex!(
+pub const DELEGATION_LAYER_PUZZLE: [u8; 881] = hex!(
   "
     ff02ffff01ff02ff12ffff04ff02ffff04ff05ffff04ff0bffff04ffff02ff5fff81bf80ffff04ff
     ff04ff17ff8080ffff04ffff02ff1effff04ff02ffff04ffff02ff1affff04ff02ffff04ff5fff80
@@ -58,27 +58,27 @@ pub const DELEGATION_LAYER_PUZZLE: [u8; 904] = hex!(
     a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba4
     71ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ffffff02ffff03ff17ffff01ff02
     ffff03ffff09ff47ff1880ffff01ff02ff12ffff04ff02ffff04ff05ffff04ff0bffff04ff37ffff
-    04ff67ffff04ff5fff8080808080808080ffff01ff04ff27ffff02ff12ffff04ff02ffff04ff05ff
-    ff04ff0bffff04ff37ffff04ff2fffff04ffff21ff5fffff02ffff03ffff09ff47ff1080ffff01ff
-    09ffff18ff820167ffff010180ffff010180ff8080ff018080ff80808080808080808080ff0180ff
-    ff01ff02ffff03ff5fff80ffff01ff04ffff04ff10ffff04ffff0bff5cffff0bff14ffff0bff14ff
-    6cff0580ffff0bff14ffff0bff7cffff0bff14ffff0bff14ff6cffff0bffff0101ff058080ffff0b
-    ff14ffff0bff7cffff0bff14ffff0bff14ff6cffff0bffff0101ff0b8080ffff0bff14ffff0bff7c
-    ffff0bff14ffff0bff14ff6cffff0bffff0101ff4f8080ffff0bff14ff6cff4c808080ff4c808080
-    ff4c808080ff4c808080ffff04ffff0101ffff04ff6fff8080808080ff808080ff018080ff0180ff
-    02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff1affff04ff02ffff04ff09ff80808080ff
-    ff02ff1affff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ffff02ff
-    ff03ff1bffff01ff02ff16ffff04ff02ffff04ffff02ffff03ffff18ffff0101ff1380ffff01ff0b
-    ffff0102ff2bff0580ffff01ff0bffff0102ff05ff2b8080ff0180ffff04ffff04ffff17ff13ffff
-    0181ff80ff3b80ff8080808080ffff010580ff0180ff02ffff03ffff09ff05ff0b80ff80ffff01ff
-    02ffff03ffff09ff17ffff02ff16ffff04ff02ffff04ffff0bffff0101ff0580ffff04ff2fff8080
-    80808080ff80ffff01ff088080ff018080ff0180ff018080
+    04ff67ffff01ff956f64645f6372656174655f636f696e5f666f756e6480808080808080ffff01ff
+    04ff27ffff02ff12ffff04ff02ffff04ff05ffff04ff0bffff04ff37ffff04ff2fffff04ff5fff80
+    808080808080808080ff0180ffff01ff02ffff03ff5fffff01ff04ffff04ff10ffff04ffff0bff5c
+    ffff0bff14ffff0bff14ff6cff0580ffff0bff14ffff0bff7cffff0bff14ffff0bff14ff6cffff0b
+    ffff0101ff058080ffff0bff14ffff0bff7cffff0bff14ffff0bff14ff6cffff0bffff0101ff0b80
+    80ffff0bff14ffff0bff7cffff0bff14ffff0bff14ff6cffff0bffff0101ff4f8080ffff0bff14ff
+    6cff4c808080ff4c808080ff4c808080ff4c808080ffff04ffff0101ffff04ff6fff8080808080ff
+    8080ff8080ff018080ff0180ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff1affff04
+    ff02ffff04ff09ff80808080ffff02ff1affff04ff02ffff04ff0dff8080808080ffff01ff0bffff
+    0101ff058080ff0180ffff02ffff03ff1bffff01ff02ff16ffff04ff02ffff04ffff02ffff03ffff
+    18ffff0101ff1380ffff01ff0bffff0102ff2bff0580ffff01ff0bffff0102ff05ff2b8080ff0180
+    ffff04ffff04ffff17ff13ffff0181ff80ff3b80ff8080808080ffff010580ff0180ff02ffff03ff
+    ff09ff05ff0b80ff80ffff01ff02ffff03ffff09ff17ffff02ff16ffff04ff02ffff04ffff0bffff
+    0101ff0580ffff04ff2fff808080808080ffff01ff0101ffff01ff088080ff018080ff0180ff0180
+    80
     "
 );
 
 pub const DELEGATION_LAYER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
   "
-    b8285eb41ad8934a5d8651d14d5fb68916f0f1d91019d0a3e1a3b1ff8a65861e
+    f5ecc72012675d5b9c7beafc1d0c81b17e7a557dfe99d24cda801da5ae75459c
     "
 ));
 
@@ -114,19 +114,19 @@ pub struct AdminFilterSolution<I> {
   pub inner_solution: I,
 }
 
-pub const ADMIN_FILTER_PUZZLE: [u8; 200] = hex!(
+pub const ADMIN_FILTER_PUZZLE: [u8; 182] = hex!(
   "
-    ff02ffff01ff02ff06ffff04ff02ffff04ffff02ff05ff0b80ff80808080ffff04ffff01ff33ff02
-    ffff03ff05ffff01ff02ffff03ffff21ffff09ff11ff0480ffff02ffff03ffff09ff11ffff0181e8
-    80ffff01ff20ffff22ffff09ff820159ff8080ffff09ff820299ffff01a057bfd1cb0adda3d94315
-    053fda723f2028320faa8338225d99f629e3d46d43a9808080ff8080ff018080ffff01ff0880ffff
-    01ff04ff09ffff02ff06ffff04ff02ffff04ff0dff808080808080ff0180ff8080ff0180ff018080
+    ff02ffff01ff02ff02ffff04ff02ffff04ffff02ff05ff0b80ff80808080ffff04ffff01ff02ffff
+    03ff05ffff01ff02ffff03ffff02ffff03ffff09ff11ffff0181e880ffff01ff22ffff09ff820159
+    ff8080ffff09ff820299ffff01a057bfd1cb0adda3d94315053fda723f2028320faa8338225d99f6
+    29e3d46d43a98080ff8080ff0180ffff01ff04ff09ffff02ff02ffff04ff02ffff04ff0dff808080
+    8080ffff01ff088080ff0180ff8080ff0180ff018080
     "
 );
 
 pub const ADMIN_FILTER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
   "
-    c8c657bbcb1af0b7ef7855cb35490ec703c034a2b085213c1463d2865a434ed1
+    ff41e0759732ab08adc419b265cfcdc2dc18115c3a6930f47f0157c5c0caa61b
     "
 ));
 
@@ -162,20 +162,20 @@ pub struct WriterFilterSolution<I> {
   pub inner_solution: I,
 }
 
-pub const WRITER_FILTER_PUZZLE: [u8; 211] = hex!(
+pub const WRITER_FILTER_PUZZLE: [u8; 201] = hex!(
   "
-    ff02ffff01ff02ff06ffff04ff02ffff04ffff02ff05ff0b80ff80808080ffff04ffff01ff33ff02
-    ffff03ff05ffff01ff02ffff03ffff21ffff09ff11ff0480ffff09ff11ffff0181f380ffff02ffff
-    03ffff09ff11ffff0181e880ffff01ff20ffff22ffff09ff820159ff8080ffff09ff820299ffff01
-    a057bfd1cb0adda3d94315053fda723f2028320faa8338225d99f629e3d46d43a9808080ff8080ff
-    018080ffff01ff0880ffff01ff04ff09ffff02ff06ffff04ff02ffff04ff0dff808080808080ff01
-    80ff8080ff0180ff018080
+    ff02ffff01ff02ff02ffff04ff02ffff04ffff02ff05ff0b80ff80808080ffff04ffff01ff02ffff
+    03ff05ffff01ff02ffff03ffff21ffff09ff11ffff0181f380ffff02ffff03ffff09ff11ffff0181
+    e880ffff01ff20ffff22ffff09ff820159ff8080ffff09ff820299ffff01a057bfd1cb0adda3d943
+    15053fda723f2028320faa8338225d99f629e3d46d43a9808080ff8080ff018080ffff01ff0880ff
+    ff01ff04ff09ffff02ff02ffff04ff02ffff04ff0dff808080808080ff0180ff8080ff0180ff0180
+    80
     "
 );
 
 pub const WRITER_FILTER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
   "
-    a2418beeba739e93a139c895c58d813de0bab2a7dc27ee4fe73927b5a6690205
+    0f3e06290983010f0c1f748a58a1d7daa4354d2298c6df7f115966881aebb0f2
     "
 ));
 
