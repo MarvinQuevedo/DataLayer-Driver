7a756d82e990e98813eae47201557c986d1338c8
yakuhito <y@kuhi.to>
2024-07-23 10:38:24 +0300

    exports test

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,5 +1,28 @@
 import test from 'ava'
 
-test('placeholder', (t) => {
-  t.is(43 - 1, 42)
+import { newLineageProof, newEveProof, Peer, selectCoins, mintStore, oracleSpend, addFee, masterPublicKeyToWalletSyntheticKey, masterPublicKeyToFirstPuzzleHash, masterSecretKeyToWalletSyntheticSecretKey, secretKeyToPublicKey, puzzleHashToAddress, addressToPuzzleHash, adminDelegatedPuzzleFromKey, writerDelegatedPuzzleFromKey, oracleDelegatedPuzzle, signCoinSpends, getCoinId, updateStoreMetadata, updateStoreOwnership, meltStore } from '../index.js';
+
+
+test('exports', (t) => {
+  t.assert(newLineageProof);
+  t.assert(newEveProof);
+  t.assert(selectCoins);
+  t.assert(mintStore);
+  t.assert(oracleSpend);
+  t.assert(addFee);
+  t.assert(masterPublicKeyToWalletSyntheticKey);
+  t.assert(masterPublicKeyToFirstPuzzleHash);
+  t.assert(masterSecretKeyToWalletSyntheticSecretKey);
+  t.assert(secretKeyToPublicKey);
+  t.assert(puzzleHashToAddress);
+  t.assert(addressToPuzzleHash);
+  t.assert(adminDelegatedPuzzleFromKey);
+  t.assert(writerDelegatedPuzzleFromKey);
+  t.assert(oracleDelegatedPuzzle);
+  t.assert(signCoinSpends);
+  t.assert(getCoinId);
+  t.assert(updateStoreMetadata);
+  t.assert(updateStoreOwnership);
+  t.assert(meltStore);
+  t.assert(Peer);
 })
